75dc4040dad16fa9530ffe0171d222b4f48e8e7f
Guada Fermani <guadalupefermani@gmail.com>
2022-06-24 03:02:35 -0300

    make banner show multiple videos

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,33 +1,19 @@
 import React from "react";
 import styles from "./Banner.module.css";
-import YouTube from "react-youtube";
+import Video from "./hooks/Video";
 
 const Banner = () => {
+        /**
+        * TODO: useApi
+      */
   return (
     <div className={styles.banner_container}>
-      <YouTube
-        videoId="oImEeiCiYTk"
-        className={styles.background_video}
-        opts={{
-          width: "100%",
-          height: "100%",
-          playerVars: {
-            autoplay: 1,
-            controls: 2,
-            mute: 1,
-            accelerometer: 1,
-            autohide: 1,
-            wmode: "opaque",
-            enablejsapi: 1,
-            origin: "http://localhost:3000",
-            display: "100%",
-          },
-        }}
-      />
+      <Video ids={["oImEeiCiYTk", "Rp7YtN_094I"]}/>
+      {/* <Video ids={["F4wgN8IMf3Q", "8koye8fa4CA", "Rp7YtN_094I"]}/> */}
       <div className={styles.banner_gradient}>
-      <div>
-        <div style={{ height: "350px" }}></div>
-      </div>
+        <div>
+          <div style={{ height: "350px" }}></div>
+        </div>
         <div className={styles.banner_title}>
           <h1>Banner</h1>
         </div>
---
+++ src/Components/Banner/hooks/Video.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./../Banner.module.css";
+import YouTube from "react-youtube";
+
+const Video = ({ids}) => {
+  const opts = {
+    width: "100%",
+    height: "100%",
+    playerVars: {
+      mute: 1,
+      autoplay: 1,
+      controls: 2,
+      accelerometer: 1,
+      autohide: 1,
+      wmode: "opaque",
+      enablejsapi: 1,
+      origin: "http://localhost:3000",
+      display: "100%",
+      playlist: [ids],
+      loop: 1,
+    },
+  };
+  return (
+    <YouTube className={styles.background_video} opts={opts} />
+  );
+};
+
+export default Video;
